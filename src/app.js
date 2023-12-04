a970100e9e0186462c42ad6491b97b64839f427b
Alex Fazakas <144437816+alexcf04@users.noreply.github.com>
2023-12-04 19:46:26 +0000

    final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function crearCarta() {
+    const numb = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    const palos = ["♠️", "♣️", "♥️", "♦️"];
+
+    const numbAleatorio = numb[Math.floor(Math.random() * numb.length)];
+    const palosAleatorio = palos[Math.floor(Math.random() * palos.length)];
+    const cartaNueva = `${numbAleatorio} ${palosAleatorio}`;
+
+    document.querySelector(`.top-card`).innerHTML = palosAleatorio;
+    document.querySelector(`.footer-card`).innerHTML = palosAleatorio;
+    document.querySelector(`.number`).innerHTML = numbAleatorio;
+    // document.getElementById(`boton`).innerHTML = "New Card";
+  }
+  crearCarta();
+  document.querySelector("#miboton").addEventListener("click", () => {
+    crearCarta();
+  });
 };
